2a036d095876024d01b6be883be8f4cb6a9005a1
hanaoka-tsujisawa <r.tsujisawa@hanaoka-corp.co.jp>
2021-05-13 18:05:20 +0900

    add map_create_post.php

--- map_create_post.php
+++ map_create_post.php
@@ -9,35 +9,25 @@
 	<body>
 		<?php
 			try {
-				// ユーザー名、パスワード
-				$username = 'postgres';
-				$password = 'admin';
+				$db = new PDO (
+					"pgsql:dbname=postgres;host=127.0.0.1;port=5432;options='--client_encoding=UTF8'",
+					"postgres",		// username
+					"admin"			// password
+				);
 
-				// データベースに接続
-				$PDO = new PDO("mysql:host=testserver;dbname=postgres;charset=utf8;",  $username,  $password );
-				// PDOのエラーレポートを表示
-				$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-				// postの値を取得
 				$time_stamping = 'CURRENT_TIMESTAMP(2)';
 				$map_name_input = $_POST['map_name_input'];
 
-				// INSERT文を変数に格納
-				$sql = "INSERT INTO test_table_map_create VALUES (:time_stamping, :map_name_input)";
-				// 挿入する値は空のまま、SQL実行の準備をする
-				$stmt = $dbh->prepare($sql);
-				// 挿入する値を配列に格納する
-				$params = array(':time_stamping' => $time_stamping, ':map_name_input' => $map_name_input, ':description' => $description);
-				// 挿入する値が入った変数をexecuteにセットしてSQLを実行
-				$stmt->execute($params);
+				$count = $db->exec(
+					'INSERT INTO test_table_map_create SET time_stamping = '.$time_stamping.', map_name='.$map_name_input.''
+				);
 
-				// 登録完了のメッセージ
 				echo "<p>time_stamping: ".$time_stamping."</p>";
-				echo "<p>map_name_input: ".$map_name_input."</p>";
+				echo "<p>map_name: ".$map_name_input."</p>";
 				echo '<p>で登録しました。</p>';
 			}
 			
-			catch (PDOException $e) {
+			catch(PDOException $e) {
 				exit('データベースに接続できませんでした。' . $e->getMessage());
 			}
 		?>
