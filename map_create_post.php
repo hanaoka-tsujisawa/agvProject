d7f361e8afd2f80e3eab0f9e0a4bbe9d3e5e3044
hanaoka-tsujisawa <82130030+hanaoka-tsujisawa@users.noreply.github.com>
2021-05-18 09:40:39 +0900

    Fix map_create_post.php

--- map_create_post.php
+++ map_create_post.php
@@ -15,20 +15,22 @@
 					"admin"			// password
 				);
 
-				$time_stamping = 'CURRENT_TIMESTAMP(2)';
 				$map_name_input = $_POST['map_name_input'];
+				$sql = "INSERT INTO test_table_map_create (map_name) VALUES ('.$map_name_input.')";
+				$res = $db->query($sql);
 
-				$count = $db->exec(
-					'INSERT INTO test_table_map_create SET time_stamping = '.$time_stamping.', map_name='.$map_name_input.''
-				);
-
-				echo "<p>time_stamping: ".$time_stamping."</p>";
 				echo "<p>map_name: ".$map_name_input."</p>";
 				echo '<p>で登録しました。</p>';
 			}
 			
 			catch(PDOException $e) {
-				exit('データベースに接続できませんでした。' . $e->getMessage());
+				$time_stamp = date("Y-m-d H:i:s") . "." . substr(explode(".", (microtime(true) . ""))[1], 0, 2);
+				$map_name_input = $_POST['map_name_input'];
+
+				echo "<p>map_name: ".$map_name_input."</p>";
+				echo "<p>データベースに接続できませんでした。</p>";
+				
+				exit($e->getMessage());
 			}
 		?>
 	</body>
